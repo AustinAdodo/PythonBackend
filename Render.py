6e05f55e2e2980e54847955a1464702a29a6ebbc
Odia <94786718+AustinAdodo@users.noreply.github.com>
2022-12-30 16:03:35 +0100

    30-12-22
    30-12-22

--- Render.py
+++ Render.py
@@ -1,18 +1,20 @@
 import textwrap
 
-n = 20
-for i in range(1, n + 1):
-    print(i, end='')
-if __name__ == '__main__':
+
+# from itertools import accumulate
+
+# for i in range(1, n + 1):
+# print(i, end='')
+def triangle_solution():
     x = int(input())
     y = int(input())
     z = int(input())
     n = int(input())
     l = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
     print(l)
-s = "qbcde"
-a = s.__len__()
-arr = {1, 2, 3, 5, 6}
+    # s = "qbcde"
+    # a = s.__len__()
+    # arr = {1, 2, 3, 5, 6}
 
 
 # arr = list(set(arr))#
@@ -39,17 +41,14 @@ def minion_game(string):
         if inp_str[i] in vowels:
             kevin_score += len_str - i
             stuart_score = total_poss_score - kevin_score
-
-    if stuart_score > kevin_score:
-        print('Stuart', stuart_score)
-
+        if stuart_score > kevin_score:
+            print('Stuart', stuart_score)
         if kevin_score == 0:
             print('Stuart', total_poss_score)
-
-    elif kevin_score > stuart_score:
-        print('Kevin', kevin_score)
-    else:
-        print('Draw')
+        elif kevin_score > stuart_score:
+            print('Kevin', kevin_score)
+        else:
+            print('Draw')
     return
 
 
@@ -61,3 +60,71 @@ def merge_the_tools(string, k):
     li = textwrap.wrap(string, k)
     for i in li:
         print(''.join(sorted(set(i), key=i.index)))
+
+
+def convert_roman(*numbers):
+    num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    ans = []
+    j = 12
+    str1 = ""
+    div = 0
+    for number in numbers:
+        while number > 0:
+            # get symbol occurrences (integer floor division)
+            div = number // num[j]
+            # and the remainder after they are handled
+            number = number % num[j]
+            # handle occurrences of the symbol
+        while div > 0:
+            str1 += sym[j]
+            div -= 1
+            j -= 1
+    ans.append(str1)
+    return ans
+
+
+def is_prime(n):
+    """
+    Assumes that n is a positive natural number
+    """
+    # We know 1 is not a prime number
+    if n == 1:
+        return False
+
+    # We store the number of factors in this variable
+    factors = 0
+    # This will loop from 1 to n
+    for i in range(1, n + 1):
+        # Check if `i` divides `n`, if yes then we increment the factors
+        if n % i == 0:
+            factors += 1
+    # If total factors are exactly 2
+    if factors == 2:
+        return True
+    return False
+
+
+def is_prime2(n):
+    """
+    Assumes that n is a positive natural number
+    """
+    # We know 1 is not a prime number
+    if n == 1:
+        return False
+
+    i = 2
+    # This will loop from 2 to int(sqrt(x))
+    while i * i <= n:
+        # Check if i divides x without leaving a remainder
+        if n % i == 0:
+            # This means that n has a factor in between 2 and sqrt(n)
+            # So it is not a prime number
+            return False
+        i += 1
+    # If we did not find any factor in the above loop,
+    # then n is a prime number
+    return True
+# test
+# for x in xrange(1, 11):
+# print '%d: %s' % (x, is_prime(x))
